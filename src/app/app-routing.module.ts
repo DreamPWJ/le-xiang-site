889ae9e53654548432e122d16c891c55c822cf02
Dream <406798106@qq.com>
2019-06-07 15:27:05 +0800

    山东乐享信息技术有限公司官网

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ const routes: Routes = [
   // pathMatch?: string; 默认为前缀匹配 "prefix"; "full" 为完全匹配
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', component: HomeComponent},
-  {path: 'login/:id', loadChildren: './pages/login/login.module#LoginComponentModule'},
+  // {path: 'login/:id', loadChildren: './pages/login/login.module#LoginComponentModule'},
+  {path: 'login/:id', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginComponentModule)},
   {path: '**', redirectTo: ''}
 ];
 
